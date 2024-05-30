57cec899658dfa491e22c67b460b14bfd16d3c1f
khairul-akmal <149129145+khairul-akmal@users.noreply.github.com>
2024-05-31 06:06:21 +0700

    home.py
    pengolahan citra khairul

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : khairul')
+st.subheader('NIM : 312210602')
+st.subheader('Kelas : TI.22.C6')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./C:\Users\khairul akmal\OneDrive\Pictures\IMG_20220707_125755_708.jpg", caption="khairul", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/chairullakmal/")
+st.link_button("Github", "https://github.com/khairul-akmal/khairul")
